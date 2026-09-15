acf084be79d3ad32f5523ef99c33487e1c5e22e3
InternationalDragon <juliaq@taltech.ee>
2026-09-15 23:37:02 +0300

    Welcome page

--- CProject/CProject/main.c
+++ CProject/CProject/main.c
@@ -2,10 +2,26 @@
 
 int main() {
 	
-	int ping;
+	int choice;
 
-	printf("This is for the first tests, ping?");
-	scanf("%d", &ping);
+	printf("Welcome! Please select an option(1-3):\n");
+	printf("1. Client\n");
+	printf("2. Cashier\n");
+	printf("3. Manager\n");
+	scanf_s("%d", &choice);
+	if (choice == 1) {
+		printf("You selected Client.\n");
+	}
+	else if (choice == 2) {
+		printf("You selected Cashier.\n");
+	}
+	else if (choice == 3) {
+		printf("You selected Manager.\n");
+	}
+	else {
+		printf("Invalid selection. Please try again.\n");
+	}
+
 
 	return 0;
 }
