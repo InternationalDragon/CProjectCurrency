af959d6a0d20c10fdbce9a2a682ae7743d63c9ab
InternationalDragon <juliaq@taltech.ee>
2026-09-16 10:16:42 +0300

    updated login page

--- CProject/CProject/main.c
+++ CProject/CProject/main.c
@@ -6,16 +6,12 @@ int main() {
 
 	printf("Welcome! Please select an option(1-3):\n");
 	printf("1. Client\n");
-	printf("2. Cashier\n");
-	printf("3. Manager\n");
+	printf("2. Manager\n");
 	scanf_s("%d", &choice);
 	if (choice == 1) {
 		printf("You selected Client.\n");
 	}
 	else if (choice == 2) {
-		printf("You selected Cashier.\n");
-	}
-	else if (choice == 3) {
 		printf("You selected Manager.\n");
 	}
 	else {
